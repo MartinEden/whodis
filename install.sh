5f3e2e9af5700baaa2d0dd0cf9ce10944954e57b
Martin Eden <martin.s.eden@pm.me>
2018-10-23 20:37:01 +0100

    Make installer better

--- install.sh
+++ install.sh
@@ -4,13 +4,18 @@ set -ex
 VERSION=1.0.0
 URL=https://github.com/MartinEden/whodis/releases/download/v${VERSION}/whodis-${VERSION}.zip
 
-apt-get install -y openjdk-8-jre nmap espeak unzip
+# Clean up any previous install
+rm /usr/bin/whodis || true
+rm -r /opt/whodis || true
+
+# Install app
+apt-get install -qq -y openjdk-8-jre nmap espeak unzip
 wget -O whodis.zip ${URL}
 unzip whodis.zip -d /opt/whodis
 ln -s /opt/whodis/whodis /usr/bin/whodis
-
 mkdir -p /etc/whodis
 
+# Add as service
 cp whodis.service /etc/systemd/system/whodis.service
 systemctl enable whodis
 systemctl start whodis

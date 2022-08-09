058b7cc944d4925d60cfcc1e9163032ce0590163
Martin Eden <martin.s.eden@pm.me>
2022-08-09 10:31:08 +0100

    Use sudo to avoid having to elevate the whole install script.

--- install.sh
+++ install.sh
@@ -4,22 +4,22 @@ set -ex
 VERSION=1.0.4
 URL=https://github.com/MartinEden/whodis/releases/download/v${VERSION}/whodis-${VERSION}.zip
 
-# Clean up any previous install
-rm /usr/bin/whodis || true
-rm -r /opt/whodis || true
+# Stop and unlink any previous install
+systemctl stop --user whodis || true
+sudo rm /usr/bin/whodis || true
 
 # Install app
-apt-get install -qq -y openjdk-8-jre nmap espeak unzip net-tools
+sudo apt-get install -qq -y openjdk-8-jre nmap espeak unzip net-tools
 wget -O whodis.zip ${URL}
-unzip whodis.zip -d /opt/whodis
-ln -s /opt/whodis/whodis-${VERSION}/bin/whodis /usr/bin/whodis
-mkdir -p /etc/whodis
+sudo unzip whodis.zip -d /opt/whodis
+sudo ln -s /opt/whodis/whodis-${VERSION}/bin/whodis /usr/bin/whodis
+sudo mkdir -p /etc/whodis
 
 # Add as service
-mv /opt/whodis/whodis-${VERSION}/whodis.service /etc/systemd/user/whodis.service
+sudo mv /opt/whodis/whodis-${VERSION}/whodis.service /etc/systemd/user/whodis.service
 systemctl enable --user whodis
+systemctl start --user whodis
 
 set +x
-echo "Installed whodis as systemd service"
+echo "Installed and started whodis as a systemd service"
 echo "Control with systemctl [start|stop|status] --user whodis"
-echo "Right now run: systemctl start --user whodis"
